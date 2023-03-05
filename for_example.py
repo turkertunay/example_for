bee1bb1d0dbd970e323c88bd57beee0262cd6e78
turkertunay <75833839+turkertunay@users.noreply.github.com>
2023-03-05 18:31:18 +0300

    Add files via upload

---
+++ for_example.py
@@ -0,0 +1,12 @@
+for letter in "Turker Tunay":
+    print(letter)
+
+letters = ["a", "e", "j", "f", "k"]
+for letter in letters:
+    print(letter)
+
+for index in range(len(letters)):
+    print(letters[index])
+
+for number in range(1,22):
+    print(number)
